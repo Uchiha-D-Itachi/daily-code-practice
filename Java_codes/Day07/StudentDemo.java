201750f042d2ffcff6019afa22a956496b624c57
Uchiha D. Itachi <uchiha2004d.itachi@gmail.com>
2026-02-10 18:49:41 +0530

    Add StudentDemo class with main method

---
+++ Java_codes/Day07/StudentDemo.java
@@ -0,0 +1,9 @@
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.id = 101;
+        s1.name = "Java Learner";
+
+        s1.display();
+    }
+}
