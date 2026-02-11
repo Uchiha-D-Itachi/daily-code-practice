17b889b856623b1841e93e4be0418717891493f2
Uchiha D. Itachi <uchiha2004d.itachi@gmail.com>
2026-02-11 06:37:08 +0530

    Add Dog class with bark method and main function

---
+++ Java_codes/Day08/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
