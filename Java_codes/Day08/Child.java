556ffa38f46eb7fe8fb8bffb07e2e4e029f82864
Uchiha D. Itachi <uchiha2004d.itachi@gmail.com>
2026-02-11 06:40:28 +0530

    Add Child class that extends Parent class

---
+++ Java_codes/Day08/Child.java
@@ -0,0 +1,16 @@
+class Parent {
+    Parent() {
+        System.out.println("Parent constructor");
+    }
+}
+
+class Child extends Parent {
+    Child() {
+        super();
+        System.out.println("Child constructor");
+    }
+
+    public static void main(String[] args) {
+        new Child();
+    }
+}
