7110b56a643c3feba20d8ca118f20517fc40e228
Uchiha D. Itachi <uchiha2004d.itachi@gmail.com>
2026-02-14 17:07:21 +0530

    Add HashMapExample class to demonstrate HashMap usage

---
+++ Java_codes/Day11/HashMapExample.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(1, "Apple");
+        map.put(2, "Banana");
+        map.put(3, "Mango");
+
+        System.out.println("HashMap Elements:");
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
