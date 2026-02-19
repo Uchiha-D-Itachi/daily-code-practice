651a4b0ad9f25811c3d97e110c922b0398c43c3c
Uchiha D. Itachi <uchiha2004d.itachi@gmail.com>
2026-02-19 18:05:42 +0530

    Create FilterExample.java

---
+++ Java_codes/Day15/FilterExample.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterExample {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30);
+
+        numbers.stream()
+               .filter(n -> n % 2 == 0)
+               .forEach(System.out::println);
+    }
+}
