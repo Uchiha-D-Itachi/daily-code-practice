4617689f804ad1b88251ecde1cf25e2f50eb03cf
Uchiha D. Itachi <uchiha2004d.itachi@gmail.com>
2026-02-21 09:29:09 +0530

    Add recursive palindrome checker

---
+++ Java_codes/Day17/PalindromeRecursive.java
@@ -0,0 +1,17 @@
+public class PalindromeRecursive {
+
+    static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end)
+            return true;
+
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        String word = "madam";
+        System.out.println(isPalindrome(word, 0, word.length() - 1));
+    }
+}
