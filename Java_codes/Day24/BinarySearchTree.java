d870e34c80c51be116e221038ee1eb2293ccf29c
Uchiha D. Itachi <uchiha2004d.itachi@gmail.com>
2026-03-02 20:41:04 +0530

    Create BinarySearchTree.java

---
+++ Java_codes/Day24/BinarySearchTree.java
@@ -0,0 +1,67 @@
+public class BinarySearchTree {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static Node insert(Node root, int key) {
+
+        if (root == null)
+            return new Node(key);
+
+        if (key < root.data)
+            root.left = insert(root.left, key);
+        else if (key > root.data)
+            root.right = insert(root.right, key);
+
+        return root;
+    }
+
+    static boolean search(Node root, int key) {
+
+        if (root == null)
+            return false;
+
+        if (root.data == key)
+            return true;
+
+        if (key < root.data)
+            return search(root.left, key);
+        else
+            return search(root.right, key);
+    }
+
+    static void inorder(Node root) {
+
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = null;
+
+        root = insert(root, 50);
+        insert(root, 30);
+        insert(root, 70);
+        insert(root, 20);
+        insert(root, 40);
+        insert(root, 60);
+        insert(root, 80);
+
+        System.out.print("Inorder (Sorted): ");
+        inorder(root);
+
+        System.out.println("\nSearch 40: " + search(root, 40));
+        System.out.println("Search 100: " + search(root, 100));
+    }
+}
